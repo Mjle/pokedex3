58734c77d27c36df292dd2719d52bff0caede928
Michael Le <mjle@ucsc.edu>
2017-04-27 21:35:23 -0700

    finished pokedex app and added loading screen

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Michael Le on 4/27/17.
+//  Copyright © 2017 Michael Le. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
